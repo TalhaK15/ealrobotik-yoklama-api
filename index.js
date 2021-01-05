8eea22a8d1606a00ea73c35e0f2308ce330c4eaf
Talha Karasu <talhafb1223@gmail.com>
2021-01-06 00:37:50 +0300

    updates for heroku deployment

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const fs = require("fs")
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // Where we will keep participants JSON.parse(fs.readFileSync("./data/participants.json")) ||
 let participants = []
