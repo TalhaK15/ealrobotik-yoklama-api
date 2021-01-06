e7875c87e59433a406b7f79323511f611eeb9626
Talha Karasu <talhafb1223@gmail.com>
2021-01-06 11:45:52 +0300

    C:/Program Files/Git/status respons with req.body

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const fs = require("fs")
-
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+// const fs = require("fs")
+// const date = new Date()
 const app = express()
 const port = process.env.PORT || 3000
+
+// let dataFileName = `data/${date.toISOString().slice(0, 10)}.json`
+// console.log()
+
+/* if(!fs.existsSync(dataFileName)) {
+  fs.writeFileSync(dataFileName, "")
+} else {
+
+} */
 
 // Where we will keep participants JSON.parse(fs.readFileSync("./data/participants.json")) ||
 let participants = []
@@ -12,27 +21,29 @@ let participants = []
 let findParticipant = (leftParticipant) => {
   return participants.map((p) => p.user_id).indexOf(leftParticipant.user_id)
 }
-
+let all
 app.use(cors())
 
 // Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.post('/join', (req, res) => {
+app.post("/join", (req, res) => {
+  all = req.body
   let joinedParticipant = req.body.payload.object.participant
   participants.push(joinedParticipant)
   res.send(participants)
 })
 
-app.post('/left', (req, res) => {
+app.post("/left", (req, res) => {
+  all = req.body
   let leftParticipant = req.body.payload.object.participant
   participants.splice(findParticipant(leftParticipant), 1)
   res.send(participants)
 })
 
-app.get('/status', (req, res) => {
-  res.send(participants)
+app.get("/status", (req, res) => {
+  res.send(all)
 })
 
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`))
